932722de401ee49320e7b85358c52f3f6ab3845f
sohagraha <sohagrahabd@gmail.com>
2021-12-26 16:25:33 +0600

    footers all options add

--- client/student/src/components/footer/Footer.js
+++ client/student/src/components/footer/Footer.js
@@ -1,11 +1,105 @@
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Link } from '@mui/material';
 import React from 'react';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 import { useStyle } from "./styles";
 
 const Footer = () => {
     const classes = useStyle();
     return (
         <div className={classes.root}>
+            <div className={classes.footers}>
+                <Container>
+                    <Grid container spacing={6}>
+                        <Grid className={classes.branding} item xs={12} md={5}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} >
+                                    <img src="https://i.ibb.co/ZWyrKZ3/image-2.png" alt="image-2" border="0" />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Gravida viverra sodales nibh quisque tincidunt. Dolor elementum elementum non vitae turpis mauris adipiscing.
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={6} md={2}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <p className={classes.heading}>Useful Links</p>
+                                    <Link underline="none" component="button">
+                                        About us
+                                    </Link>
+                                    <br />
+                                    <Link underline="none" component="button">
+                                        Work with us
+                                    </Link>
+                                    <br />
+                                    <Link underline="none" component="button">
+                                        Love Mondays
+                                    </Link>
+                                    <br />
+                                    <Link underline="none" component="button">
+                                        For developers
+                                    </Link>
+                                    <Link underline="none" component="button">
+                                        Press office
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={6} md={2}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <p className={classes.heading}>Courses</p>
+                                    <Link underline="none" component="button">
+                                        Find a course
+                                    </Link>
+                                    <br />
+                                    <Link underline="none" component="button">
+                                        Career guides
+                                    </Link>
+                                    <br />
+                                    <Link underline="none" component="button">
+                                        Discount courses
+                                    </Link>
+                                    <br />
+                                    <Link underline="none" component="button">
+                                        Online courses
+                                    </Link>
+                                    <Link underline="none" component="button">
+                                        Free courses
+                                    </Link>
+                                    <Link underline="none" component="button">
+                                        Discount codes
+                                    </Link>
+                                    <Link underline="none" component="button">
+                                        On Demand Terms
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} md={3}>
+                            Contact US
+                            <p className={classes.contact}><LocalPhoneIcon className={classes.ico} />  +00 123 456 789</p>
+                            <p className={classes.contact}><MailOutlineIcon className={classes.ico} />  info@udemy.inc</p>
+                            <p className={classes.contact}><AddLocationIcon className={classes.ico} />  lorem ipsum Road,
+                                London, United Kingdom</p>
+                            <div className={classes.icons}>
+                                <FacebookIcon className={classes.icon}></FacebookIcon>
+                                <TwitterIcon className={classes.icon}></TwitterIcon>
+                                <InstagramIcon className={classes.icon}></InstagramIcon>
+                                <YouTubeIcon className={classes.icon}></YouTubeIcon>
+                                <PinterestIcon className={classes.icon}></PinterestIcon>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
             <div className={classes.footer}>
                 <Container>
                     <Grid container spacing={2}>
--- client/student/src/components/footer/styles.js
+++ client/student/src/components/footer/styles.js
@@ -2,7 +2,26 @@ import { makeStyles } from "@mui/styles";
 
 export const useStyle = makeStyles({
     root: {
+        marginTop: "50px",
         flexGrow: 1,
+
+    },
+    footers: {
+        paddingBottom: "10px",
+        backgroundColor: "#FFF1F1",
+    },
+    branding: {
+        display: "flex",
+        alignItems: "center",
+    },
+    heading: {
+        margin: "0px",
+        padding: "0px",
+        fontWeight: 400,
+        fontSize: "16px",
+        fontFamily: "mulish",
+        paddingBottom: "10px",
+
     },
     footer: {
         marginTop: "20px",
@@ -10,5 +29,19 @@ export const useStyle = makeStyles({
         backgroundColor: "#EA5252",
         textAlign: "center",
         color: "white",
+    },
+    ico: {
+        color: "#EA5252",
+    },
+    contact: {
+        display: "flex",
+        alignItems: "center",
+    },
+    icons: {
+        color: "#EA5252",
+    },
+    icon: {
+        margin: "6px",
+        border: "0.5px solid #EA5252",
     },
 });
