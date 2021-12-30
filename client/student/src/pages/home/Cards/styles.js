748e3428411357949b12c062b0b6d4903965e123
rashed abir <rashed15-2155@diu.edu.bd>
2021-12-30 23:35:45 +0600

    branch merge && navbar design update

--- client/student/src/pages/home/Cards/styles.js
+++ client/student/src/pages/home/Cards/styles.js
@@ -1,40 +1,39 @@
 import { makeStyles } from "@mui/styles";
-import { margin } from "@mui/system";
 
 export const useStyle = makeStyles({
-    root: {
-        maxWidth: "100%",
-        margin: "5px",
-    },
-    tag: {
-        backgroundColor: "orange",
-        color: "white",
-        outline: "none",
-        outlineColor: "yellow",
-    },
-    heading: {
-        fontSize: "14px",
-        padding: "0px 0px 0px 15px",
-    },
-    avatar: {
-        padding: "0px 0px 0px 8px",
-    },
-    flexitem: {
-        display: "flex",
-        alignItems: "center"
-    },
-    gridcontainer: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        text: "center",
-        fontSize: "12px",
-        fontWeight: 700,
-        paddingBottom: "5px",
-    },
-    linecut: {
-        display: "flex",
-        alignItems: "center",
-        textDecoration: "line-through",
-    }
-})
+  root: {
+    maxWidth: "100%",
+    margin: "5px",
+  },
+  tag: {
+    backgroundColor: "orange",
+    color: "white",
+    outline: "none",
+    outlineColor: "yellow",
+  },
+  heading: {
+    fontSize: "14px",
+    padding: "0px 0px 0px 15px",
+  },
+  avatar: {
+    padding: "0px 0px 0px 8px",
+  },
+  flexitem: {
+    display: "flex",
+    alignItems: "center",
+  },
+  gridcontainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    text: "center",
+    fontSize: "12px",
+    fontWeight: 700,
+    paddingBottom: "5px",
+  },
+  linecut: {
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "line-through",
+  },
+});
