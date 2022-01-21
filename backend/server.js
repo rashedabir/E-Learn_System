45a1150bd22a22f9fde42d6d87166e89cbb26020
Rashed Khan <rashed15-2155@diu.edu.bd>
2022-01-21 11:23:19 +0600

    test

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
-const path = require("path");
+// const path = require("path");
 
 const app = express();
 app.use(express.json());
