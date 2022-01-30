84250ec49173abb2d384b2f7469999fc6c2b1c73
Rashed Khan <rashed15-2155@diu.edu.bd>
2022-01-30 20:44:09 +0600

    dashboard tab

--- client/student/src/components/Cards/Card.js
+++ client/student/src/components/Cards/Card.js
@@ -1,42 +1,53 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import { Grid } from '@mui/material';
+import React from "react";
+import Card from "@mui/material/Card";
+import { Grid } from "@mui/material";
 import { useStyle } from "./styles";
-import StarRateIcon from '@mui/icons-material/StarRate';
-import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import { Link } from "react-router-dom";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import PeopleOutlineIcon from "@mui/icons-material/PeopleOutline";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 
-
-const Cards = () => {
+const Cards = ({ item }) => {
   const classes = useStyle();
   return (
     <div className={classes.root}>
-      <Card sx={{ border: "none", boxShadow: "none", }}>
-        <img className={classes.cardimg} width="100%" src="https://picsum.photos/800/600?random=2" alt="" />
-        <button className={classes.tag}>Beginer</button>
-        <h1 className={classes.heading}>Python and Django Full Stack Web Developer Bootcamp</h1>
+      <Card sx={{ border: "none", boxShadow: "none" }}>
+        <img
+          className={classes.cardimg}
+          width="100%"
+          src={item?.banner?.url || "https://picsum.photos/800/600?random=2"}
+          alt=""
+        />
+        <button className={classes.tag}>{item?.category}</button>
+        <h1 className={classes.heading}>
+          <Link to={`/course_details/${item?._id}`}>{item?.title}</Link>
+        </h1>
         <div className={classes.flexitem}>
           <div className={classes.avatar}>
-            <img className={classes.avatarimg} src="https://picsum.photos/800/600?random=2" alt="" />
+            <img
+              className={classes.avatarimg}
+              src="https://picsum.photos/800/600?random=2"
+              alt=""
+            />
           </div>
-          <p className={classes.avatar}>Sohag Raha</p>
+          <p className={classes.avatar}>{item?.instructor?.name}</p>
         </div>
         <Grid className={classes.gridcontainer} container spacing={2}>
           <Grid className={classes.flexitem} item xs={3}>
-            <StarRateIcon className={classes.star} /> <span className={classes.startext}>4.6</span>
+            <StarRateIcon className={classes.star} />{" "}
+            <span className={classes.startext}>4.6</span>
           </Grid>
           <Grid className={classes.flexitem} item xs={3}>
-            <PeopleOutlineIcon className={classes.enroll} /> <span className={classes.enrolltxt}>1200+</span>
+            <PeopleOutlineIcon className={classes.enroll} />{" "}
+            <span className={classes.enrolltxt}>1200+</span>
           </Grid>
           <Grid className={classes.flexitems} item xs={4}>
-            <span className={classes.normal}><AttachMoneyIcon className={classes.dollar} /><span className={classes.pricetxt}>58.60</span></span>
+            <span className={classes.normal}>
+              <AttachMoneyIcon className={classes.dollar} />
+              <span className={classes.pricetxt}>{item?.price}</span>
+            </span>
           </Grid>
-          {/* <Grid className={classes.flexitem} item xs={3}>
-            <span className={classes.linecut}><AttachMoneyIcon className={classes.dollarcut} /><span className={classes.pricetxt}>58.60</span></span>
-          </Grid> */}
-
         </Grid>
-
       </Card>
     </div>
   );
