368c46fe8022cf2e2b038dc5e8f75f0e94fbe670
Rashed Khan <rashed15-2155@diu.edu.bd>
2022-02-01 19:05:36 +0600

    lesson

--- client/student/src/pages/instrucrorDashboard/singleCourse/lessons/Lesson.js
+++ client/student/src/pages/instrucrorDashboard/singleCourse/lessons/Lesson.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import LessonVideo from './lessonsVideo/LessonVideo';
+import React from "react";
+import LessonVideo from "./lessonsVideo/LessonVideo";
 import { useStyle } from "./styles";
 
 const Lesson = (props) => {
-    const classes = useStyle();
-    const { title, videos } = props.lessons;
-    return (
-        <div>
-            <h3 className={classes.lessonhead}>Lesson : {title}</h3>
-            {
-                videos.map(video => <LessonVideo videos={video} key={video.id}></LessonVideo>)
-            }
-        </div>
-    );
+  const classes = useStyle();
+  const { title, videos } = props.lessons;
+  return (
+    <div className={classes.lessonWrapper}>
+      <h3 className={classes.lessonhead}>Heading : {title}</h3>
+      {videos.map((video) => (
+        <LessonVideo videos={video} key={video.id}></LessonVideo>
+      ))}
+    </div>
+  );
 };
 
 export default Lesson;
--- client/student/src/pages/instrucrorDashboard/singleCourse/lessons/styles.js
+++ client/student/src/pages/instrucrorDashboard/singleCourse/lessons/styles.js
@@ -1,14 +1,17 @@
 import { makeStyles } from "@mui/styles";
 
 export const useStyle = makeStyles({
-    root: {
-
-    },
-    lessonhead: {
-        backgroundColor: "#d7d9d7",
-        color: 'black',
-        padding: '4px',
-        marginBottom: '6px',
-        marginTop: '6px',
-    },
+  root: {},
+  lessonhead: {
+    backgroundColor: "#d7d9d7",
+    color: "black",
+    padding: "4px",
+    marginBottom: "6px",
+    marginTop: "6px",
+  },
+  lessonWrapper: {
+    background: "#EEE",
+    margin: "20px 0",
+    padding: "20px",
+  },
 });
