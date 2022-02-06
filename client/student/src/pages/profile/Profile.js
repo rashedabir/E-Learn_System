5a832669355246cf1d05eb454e2a58c5afe3c770
sohagraha <sohagrahabd@gmail.com>
2022-02-06 13:04:51 +0600

    task

--- client/student/src/pages/profile/Profile.js
+++ client/student/src/pages/profile/Profile.js
@@ -9,7 +9,7 @@ const Profile = () => {
             <Grid container spacing={2}>
                 <Grid className={classes.profileleft} item xs={3}>
                     <img className={classes.imag} src="https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo=" alt="" />
-                    <h4>Name : Sohag Raha</h4>
+                    <h4>Name : Sohag Rah</h4>
                 </Grid>
                 <Grid className={classes.profileright} item xs={9}>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. At illum, natus hic nisi iure possimus. Recusandae repellat facilis est rem molestias sequi nobis impedit, corporis minus at nostrum aliquid! Dignissimos.
