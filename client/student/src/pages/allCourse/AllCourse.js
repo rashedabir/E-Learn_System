f5a0a0ea579af1e8e4f422842f313a09f5265e4e
Rashed Khan <rashed15-2155@diu.edu.bd>
2022-02-12 12:38:12 +0600

    all course show with set filter

---
+++ client/student/src/pages/allCourse/AllCourse.js
@@ -0,0 +1,42 @@
+import { Button, Container, Grid, Grow } from "@mui/material";
+import React, { useContext } from "react";
+import Filter from "../../components/filter/Filter";
+import { GlobalState } from "../../GlobalState";
+import Cards from "../../components/Cards/Card";
+import CourseCard from "../../components/skeleton/CourseCard";
+
+const AllCourse = () => {
+  const state = useContext(GlobalState);
+  const [courses] = state.courseAPI.courses;
+  const [loading] = state.courseAPI.loading;
+  const [page, setPage] = state.courseAPI.page;
+  const [result] = state.courseAPI.result;
+  return (
+    <div>
+      <Container maxWidth="xl">
+        <Filter />
+        <Grow in>
+          <Grid container spacing={3} alignContent="stretch">
+            {courses.map((course, i) => (
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Cards key={i} item={course} type="details" />
+              </Grid>
+            ))}
+          </Grid>
+        </Grow>
+        {courses.length === 0 && <CourseCard loading={loading} />}
+        <div className="load_more">
+          {result < page * 8 ? (
+            ""
+          ) : (
+            <Button color="primary" onClick={() => setPage(page + 1)}>
+              Load more
+            </Button>
+          )}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default AllCourse;
