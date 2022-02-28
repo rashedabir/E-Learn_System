b2fa103190aaef668ce5019e3a2d4074a13b0c9a
sohagraha <sohagrahabd@gmail.com>
2022-02-28 19:29:23 +0600

    submit reply answer

--- client/student/src/pages/courseDiscussion/CourseDiscussion.js
+++ client/student/src/pages/courseDiscussion/CourseDiscussion.js
@@ -1,7 +1,9 @@
-import { Container } from "@mui/material";
+import { Button, Container, ListItemText, TextField } from "@mui/material";
+import { Box } from "@mui/system";
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import { GlobalState } from "../../GlobalState";
 import { useStyle } from "./styles";
 
@@ -37,9 +39,62 @@ const CourseDiscussion = () => {
     }
   }, [discussionId, token]);
 
+  const submitReply = async () => {
+    try {
+      await axios.post(
+        `https://e-learn-bd.herokuapp.com/api/discussion/single/${discussionId}`,
+        {
+          answer: reply,
+          type: user.type,
+        },
+        {
+          headers: { Authorization: token },
+        }
+      );
+      toast.success("Submitted");
+    } catch (error) {
+      toast.error(error.response.data.msg);
+    }
+  };
+
   return (
     <div className={classes.root}>
-      <Container maxWidth="xl">CourseDiscussion</Container>
+      <Container maxWidth="xl">
+        <Box className={classes.discription}>
+          <ListItemText
+            primary={`Qestion : ${discussion?.question}`}
+            secondary={`Posted ${new Date(
+              discussion?.createdAt
+            ).toDateString()}`}
+          />
+          <TextField
+            id="outlined-multiline-static"
+            label="Answer Question"
+            multiline
+            rows={4}
+            variant="outlined"
+            fullWidth
+            color="warning"
+            className={classes.answerField}
+            onChange={(e) => {
+              setReply(e.target.value);
+            }}
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            style={{
+              backgroundColor: "#EA5252",
+              textTransform: "none",
+            }}
+            sx={{ mt: 5 }}
+            onClick={submitReply}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Container>
+
     </div>
   );
 };
