45daea8909ff26aa7cafa84dcde09016d742a124
Rashed Khan <rashed15-2155@diu.edu.bd>
2022-02-28 19:19:00 +0600

    payment system

--- client/student/src/pages/home/enroll/enroll.js
+++ client/student/src/pages/home/enroll/enroll.js
@@ -1,24 +1,57 @@
-import { Button, Divider, Typography } from "@mui/material";
+import {
+  Backdrop,
+  Box,
+  Button,
+  Divider,
+  Fade,
+  Modal,
+  Typography,
+} from "@mui/material";
+import {
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GlobalState } from "../../../GlobalState";
 import { useStyles } from "./styles";
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #EA5252',
+  bgcolor: "background.paper",
+  border: "2px solid #fff",
   boxShadow: 24,
+  borderRadius: "6px",
   p: 4,
 };
 
+const stripePromise = loadStripe(
+  "pk_test_51JIDmDAcmD9cnihVfUC3Z06F9HJyqVKaUIl6UhDBF5HcbgR8T5PKLnPiDhjJf6wz4H1Lk7ZMiAWAW50Th3VwA6Q600zZG1YIim"
+);
+
+const handleSubmit = (stripe, elements) => async () => {
+  const cardElement = elements.getElement(CardElement);
+
+  const { error, paymentMethod } = await stripe.createPaymentMethod({
+    type: "card",
+    card: cardElement,
+  });
+
+  if (error) {
+    console.log("[error]", error);
+  } else {
+    console.log("[PaymentMethod]", paymentMethod);
+    // ... SEND to your API server to process payment intent
+  }
+};
 
 const EnrollStudent = () => {
   const classes = useStyles();
@@ -30,9 +63,37 @@ const EnrollStudent = () => {
   const [enrolled, setEnrolled] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => { setOpen(true) };
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    return (
+      <>
+        <CardElement />
+        {/*<button onClick={handleSubmit(stripe, elements)}>Buy</button>*/}
+        <Button
+          style={{
+            backgroundColor: "#EA5252",
+            padding: "18px 25px",
+            fontSize: "17px",
+            textTransform: "none",
+            marginTop: "55px",
+          }}
+          className={classes.button}
+          variant="contained"
+          fullWidth
+          onClick={() => {
+            addList(course);
+          }}
+        >
+          {enrolled ? "Enrolled" : "Confirm Enroll"}
+        </Button>
+      </>
+    );
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -110,24 +171,7 @@ const EnrollStudent = () => {
           <Button
             style={{
               backgroundColor: "#EA5252",
-              padding: "18px 25px",
-              fontSize: "17px",
-              textTransform: "none",
-              marginTop: "25px",
-            }}
-            className={classes.button}
-            variant="contained"
-            onClick={() => {
-              addList(course);
-            }}
-          >
-            {enrolled ? "Enrolled" : "Confirm Enroll"}
-          </Button>
-
-          <Button
-            style={{
-              backgroundColor: "#EA5252",
-              padding: "18px 25px",
+              padding: "10px 25px",
               fontSize: "17px",
               textTransform: "none",
               marginTop: "25px",
@@ -136,25 +180,49 @@ const EnrollStudent = () => {
             variant="contained"
             onClick={handleOpen}
           >
-            pay
+            Checkout
           </Button>
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Text in a modal
-              </Typography>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-              </Typography>
-            </Box>
-          </Modal>
         </form>
       )}
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography
+                sx={{ pb: 5 }}
+                id="transition-modal-title"
+                variant="h5"
+                component="h1"
+              >
+                Payment
+              </Typography>
+              <Typography
+                className={classes.price}
+                sx={{ pb: 5 }}
+                id="transition-modal-title"
+                variant="h5"
+                component="h1"
+              >
+                ${course?.courseDetails?.price}
+              </Typography>
+            </Box>
+            <Elements stripe={stripePromise}>
+              <PaymentForm />
+            </Elements>
+          </Box>
+        </Fade>
+      </Modal>
     </div>
   );
 };
