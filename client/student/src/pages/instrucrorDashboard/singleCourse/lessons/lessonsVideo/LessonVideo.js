7a3f92207079bf5bc59ff6189d72c66cdf02e1f9
sohagraha <sohagrahabd@gmail.com>
2022-03-10 23:04:55 +0600

    video lesson show fix

--- client/student/src/pages/instrucrorDashboard/singleCourse/lessons/lessonsVideo/LessonVideo.js
+++ client/student/src/pages/instrucrorDashboard/singleCourse/lessons/lessonsVideo/LessonVideo.js
@@ -1,16 +1,17 @@
 import React from "react";
+import ReactPlayer from "react-player";
 import { useStyle } from "./styles";
 
 const LessonVideo = (props) => {
-  // const { title, link } = props.videos;
-  const { title } = props.videos;
+  const { title, link } = props.videos;
+  // const { title } = props.videos;
   const classes = useStyle();
   return (
     <div className={classes.root}>
       <div className={classes.title}>
         <span>Class : {title}</span>
       </div>
-      {/* <ReactPlayer url={link} /> */}
+      <ReactPlayer url={link} />
     </div>
   );
 };
